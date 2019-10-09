5daea6f8024a269a1e617e015a0549f63bd21cb0
Matt <mstewz3646@gmail.com>
2019-10-09 14:39:15 -0700

    Chart is finally working. Reset button is super buggy though.

--- js/app.js
+++ js/app.js
@@ -123,22 +123,27 @@ var handleClickOnImg = function(event) {
   }  
   totalClicks++;
   if(totalClicks === rounds){
-    imgDivTag.removeEventListener('click', handleClickOnImg);
     for (var i = 0; i < groupImages.allImages.length; i++) {
       var liData = document.createElement('li');
       liData.textContent = `${groupImages.allImages[i].name}: ${groupImages.allImages[i].clicks} total clicks`;
       ul.appendChild(liData);
+      // resetButton.addEventListener('click',handleReset);
+      // resetButton.className = 'resetEnable';
+      // imageSectionTag.removeEventListener('click', handleClickOnImg);
+      console.log('click',handleClickOnImg);
+      console.log('You have seen 25 images, thanks for your time!');
+      makeImageChart();
       }
     }
   };
 
 imgDivTag.addEventListener('click', handleClickOnImg);
 
-// RESET BUTTON
+// RESET BUTTON NOT WORKING
 // function handleReset(){
 //   totalClicks = 0;
 //   imageSectionTag.addEventListener('click', handleCLickOnImg);
-//   resetButton.className = 'resetDisable';
+//   resetButton.classname = 'resetDisable';
 // }
 
 new groupImages('bag', './img/bag.jpg');
@@ -186,40 +191,43 @@ var genData = function(images) {
   
 };
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-type: 'bar',
-data: {
-  labels: genLabels(groupImages.allImages),
-  datasets: [{
-    label: '# of Votes',
-    data: genData(groupImages.allImages),
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(255, 206, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(255, 159, 64, 0.2)'
-    ],
-    borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-    ],
-  borderWidth: 1
-  }]
-},
-options: {
-  scales: {
-    yAxes: [{
-      ticks: {
-        beginAtZero: true
-        }
+function makeImageChart(){
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: genLabels(groupImages.allImages),
+      datasets: [{
+        label: '# of Votes',
+        data: genData(groupImages.allImages),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+      borderWidth: 1
       }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
     }
-  }
-});
+  });
+}
